28b34ab9292a1f427038b6955592d5e7b94e0750
caduzinhoo <carloseduardo60224@gmail.com>
2026-03-11 03:37:06 -0300

    quinta atualização

--- lib/src/home/home_tab.dart
+++ lib/src/home/home_tab.dart
@@ -1,10 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom-colors.dart';
 
 class HomeTab extends StatelessWidget {
   const HomeTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      // app Bar
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: Text.rich(
+          TextSpan(
+            style: TextStyle(fontSize: 30),
+
+            children: [
+              TextSpan(
+                text: "Green",
+                style: TextStyle(color: CustomColors.customSwatchColor),
+              ),
+
+              TextSpan(
+                text: "Grocer",
+                style: TextStyle(color: CustomColors.customContrastColor),
+              ),
+            ],
+          ),
+        ),
+
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 15, 
+              right: 15,
+              ),
+
+            child: GestureDetector(
+              onTap: () {},
+
+              child: Badge(
+                backgroundColor: CustomColors.customContrastColor,
+                label: const Text('2', 
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10,
+                  ),
+                 ),
+                child: Icon(Icons.shopping_cart, color: CustomColors.   customSwatchColor),
+              ),
+            ),
+          ),
+        ],
+      ),
+   
+        body: Column(
+          children: [
+          // Campo de Pesquisa
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+            child: TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                isDense: true,
+                hintText: 'Pesquise aqui...',
+                hintStyle: TextStyle(
+                  color: Colors.grey.shade400,
+                  fontSize: 14,
+            
+                ),
+                prefixIcon:
+                  Icon(Icons.search, 
+                  color: CustomColors.customContrastColor,  
+                  size: 21 ,
+                  ),
+            
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(60),
+                  borderSide: BorderSide(
+                    width: 0,
+                    style: BorderStyle.none,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // Categorias
+
+          // Grid
+        ],
+      ),     
+   );
   }
 }
