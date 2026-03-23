ed625dc9705eb9e972abed1b8e4eab1f675c22e8
caduzinhoo <carloseduardo60224@gmail.com>
2026-03-23 15:20:21 -0300

    atualização as 15:20

--- lib/src/pages/profile/profile_tab.dart
+++ lib/src/pages/profile/profile_tab.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:greengrocer/src/pages/auth/components/custom_text_fiel.dart';
 import 'package:greengrocer/src/config/app_data.dart' as appData;
 
- class ProfileTab extends StatelessWidget {
+ class ProfileTab extends StatefulWidget {
   const ProfileTab({super.key});
 
+  @override
+  State<ProfileTab> createState() => _ProfileTabState();
+}
+
+class _ProfileTabState extends State<ProfileTab> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,6 +72,38 @@ import 'package:greengrocer/src/config/app_data.dart' as appData;
 
         ],
       ),
+    );
+  }
+
+  Future<bool?>updatePassword(){
+    return showDialog(
+      context: context,
+      builder:(context){
+        return Dialog( 
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+
+
+              CustomTextFiel(
+                icon: Icons.lock,
+                label: 'Senha atual',  
+                ),
+              CustomTextFiel(
+                icon: Icons.lock,
+                label: 'Nova senha',  
+                ),
+
+              CustomTextFiel(
+                icon: Icons.lock, 
+                label: 'Confirmar nova senha',  
+                ),
+            
+            
+            ],
+          ),
+        );
+      } 
     );
   }
 }
