760b08f5e3e44eb45627bb76eebb0c47688fec80
caduzinhoo <carloseduardo60224@gmail.com>
2026-03-24 08:12:25 -0300

    atualização as 08h

--- lib/src/pages/orders/components/order_status_widget.dart
+++ lib/src/pages/orders/components/order_status_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom-colors.dart';
-
 class OrderStatusWidget extends StatelessWidget {
   final String status;
   final bool isOverdue;
@@ -15,7 +14,11 @@ class OrderStatusWidget extends StatelessWidget {
 
   int get currentStatus => allStatus[status]!;
 
-  OrderStatusWidget({required this.isOverdue, required this.status, required });
+  OrderStatusWidget({super.key, 
+    required this.isOverdue, 
+    required this.status,
+        
+    });
 
   @override
   Widget build(BuildContext context) {
@@ -23,20 +26,68 @@ class OrderStatusWidget extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
 
       children: [
-        _StatusDot(isActive: true, title: 'Pedido confirmado'),
+
+        _StatusDot(
+          isActive: true, 
+          title: 'Pedido confirmado', 
+          backgroundColor: null,
+          ),
 
         _CustomDivider(),
 
         if (currentStatus == 1) ...[
-          _StatusDot(isActive: true, title: 'Pix estornado'),
-        ],
+          _StatusDot(
+            isActive: true, 
+            title: 'Pix estornado', 
+            backgroundColor: Colors.orange,
+          ),
+        ] else if(isOverdue) ...[ 
+          _StatusDot(
+          isActive: true, 
+          title: 'Pagamento Pix vencido',
+          backgroundColor: Colors.red,
+          ),
+
+        ] else ...[
+          _StatusDot(
+          isActive: currentStatus >= 2, 
+          title: 'Pagamento',
+          backgroundColor: null,
+          ),
+
+          _CustomDivider(),
+
+          _StatusDot(
+          isActive: currentStatus >= 3, 
+          title: 'Preparando',
+          backgroundColor: null,
+          ),
+
+          _CustomDivider(),
+
+          _StatusDot(
+          isActive: currentStatus >= 4, 
+          title: 'Envio',
+          backgroundColor: null,
+          ),
+
+          _CustomDivider(),
+
+          _StatusDot(
+          isActive: currentStatus == 5, 
+          title: 'Entregue',
+          backgroundColor: null,
+          ),
+
+
+        ]
       ],
     );
   }
 }
 
 class _CustomDivider extends StatelessWidget {
-  const _CustomDivider({super.key});
+  const _CustomDivider();
 
   @override
   Widget build(BuildContext context) {
@@ -69,7 +120,7 @@ class _StatusDot extends StatelessWidget {
           alignment: Alignment.center,
           height: 20,
           width: 20,
-          decoration: BoxDecoration(
+           decoration: BoxDecoration(
             shape: BoxShape.circle,
             border: Border.all(color: CustomColors.customSwatchColor),
 
@@ -85,7 +136,9 @@ class _StatusDot extends StatelessWidget {
         SizedBox(width: 5),
 
         // Texto
-        Expanded(child: Text(title, style: TextStyle(fontSize: 12))),
+        Expanded(child: Text(title, style: TextStyle(fontSize: 12),
+         ),
+        ),
       ],
     );
   }
--- lib/src/pages/orders/components/order_tile.dart
+++ lib/src/pages/orders/components/order_tile.dart
@@ -23,6 +23,8 @@ class OrderTile extends StatelessWidget {
       child: Theme(
         data:Theme.of(context).copyWith(dividerColor: Colors.transparent),
         child: ExpansionTile(
+          initiallyExpanded: order.status == 'pending_payment',
+
           title: Column(
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -40,32 +42,34 @@ class OrderTile extends StatelessWidget {
           childrenPadding: EdgeInsets.fromLTRB(16, 0, 16, 16), 
           children: [
 
-            SizedBox(
-              height: 150,
+            IntrinsicHeight(
               child: Row(
                 children: [
-
+              
                   // lista de produtos
                   Expanded(
                     flex: 3,
-                    child: ListView(
-                      children: order.items.map((orderItem) { 
-                        return _OrderItemWidget(utilsServices: utilsServices, orderItem: orderItem,
-                        );
-                        
-                      }).toList(),
+                    child: SizedBox(
+                      height: 150,
+                      child: ListView(
+                        children: order.items.map((orderItem) { 
+                          return _OrderItemWidget(utilsServices: utilsServices, orderItem: orderItem,
+                          );
+                          
+                        }).toList(),
+                      ),
                     )
                       ),
               
-
+              
                   // Divisão 
                   VerticalDivider(
                     color: Colors.grey.shade400,
                     thickness: 2,
                     width: 10,
                   ), 
-
-
+              
+              
                   // status do pedido 
                   Expanded(
                     flex: 2,
